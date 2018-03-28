f28c14b7ef2d346a7ccd14bc4b495143ba42b84e
Anton von Schantz <anton.von.schantz@aalto.fi>
2018-03-28 13:06:38 +0300

    Add files via upload

--- data_analysis/figure_2.py
+++ data_analysis/figure_2.py
@@ -15,119 +15,220 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 width = 20
 height = 20
 
-# Number of pedestrians in room initially
-n_a = 1000
+# Names of folders containing the strategy data for different scenarios.
+mylist = ['taset0', 'taset80', 'taset150', 'taset500']
 
-# Number of pedestrians exited
-stat_reg_start = 9
+# Figure titles
+titles = [r'$T_{ASET}$=0', r'$T_{ASET}$=80', r'$T_{ASET}$=150', r'$T_{ASET}$=500']
 
 # Initialize figure
 fig = plt.figure(0)
+grid = ImageGrid(fig, 111, nrows_ncols=(2,2), axes_pad=0.35, aspect=True)
 
 # Create a color map of fixed colors
 cmap = colors.ListedColormap(['red', 'blue'])
 bounds=[0,0.5,1]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 
-# Load data of which pedestrians are in the room
-if os.path.exists('bigequilibrium/in_room1.npy.gz'):
-    in_room = np.loadtxt('bigequilibrium/in_room1.npy.gz')
-    sum_in_room = np.sum(in_room, axis=1)
+# The simulation which data is used to plot the equilibria (qualitatively the equilibria for different
+# simulations look the same.
+j = 99
 
-    # Time when n_a - stat_reg_start pedestrians have exited
-    time_stat_reg_start = np.where(sum_in_room == (n_a -stat_reg_start))[0][0]
-    agents_in_room = np.where(in_room[time_stat_reg_start, :] == 1)[0]
+# Loop through the different scenarios
+for i in range(0,4):
 
-# Load pedestrian's x-positions
-if os.path.exists('bigequilibrium/positions_x.npy.gz'):
-    positions_x = np.loadtxt('bigequilibrium/positions_x.npy.gz')
-    positions_x = positions_x[0::2]
+    # Load data of which pedestrians are in the room
+    if os.path.exists("{}{}{}{}{}".format(mylist[i], '/', 'in_room1', j, '.npy.gz')):
+        in_room = np.loadtxt("{}{}{}{}{}".format(mylist[i], '/', 'in_room1', j, '.npy.gz'))
+        sum_in_room = np.sum(in_room, axis=1)
 
-# Load pedestrian's y-positions
-if os.path.exists('bigequilibrium/positions_y.npy.gz'):
-    positions_y = np.loadtxt('bigequilibrium/positions_y.npy.gz')
-    positions_y = positions_y[0::2]
+        # Time when 10 pedestrians have exited
+        time_stat_reg_start = np.where(sum_in_room == 190)[0][0]
 
-# Load pedestrian's radii
-if os.path.exists('bigequilibrium/radius.npy.gz'):
-    radius = np.loadtxt('bigequilibrium/radius.npy.gz')
+        # Pedestrians in room when 10 pedestrians have exited
+        agents_in_room = np.where(in_room[time_stat_reg_start, :] == 1)[0]
 
-# Load pedestrian's strategies
-if os.path.exists('bigequilibrium/strategy.npy.gz'):
-    strategy = np.loadtxt('bigequilibrium/strategy.npy.gz')
+    # Load pedestrian's x-positions
+    if os.path.exists("{}{}{}{}{}".format(mylist[i], '/', 'positions_x', j, '.npy.gz')):
+        positions_x = np.loadtxt("{}{}{}{}{}".format(mylist[i], '/', 'positions_x', j, '.npy.gz'))
+        positions_x = positions_x[0::2]
 
-# Create cricles based on pedestrian's positions and radius
-patches = []
-for k in agents_in_room:
-    circle = ptch.Circle((positions_y[time_stat_reg_start, k], -positions_x[time_stat_reg_start, k] + width),
+        # Which pedestrians have exited the room
+        outside_room = np.where(in_room[time_stat_reg_start, :] == 0)[0]
+
+    # Load pedestrian's y-positions
+    if os.path.exists("{}{}{}{}{}".format(mylist[i], '/', 'positions_y', j, '.npy.gz')):
+        positions_y = np.loadtxt("{}{}{}{}{}".format(mylist[i], '/', 'positions_y', j, '.npy.gz'))
+        positions_y = positions_y[0::2]
+
+    # Load pedestrian's radii
+    if os.path.exists("{}{}{}{}{}".format(mylist[i], '/', 'radius', j, '.npy.gz')):
+        radius = np.loadtxt("{}{}{}{}{}".format(mylist[i], '/', 'radius', j, '.npy.gz'))
+
+    # Load pedestrian's strategies
+    if os.path.exists("{}{}{}{}{}".format(mylist[i], '/', 'strategy', j, '.npy.gz')):
+        strategy = np.loadtxt("{}{}{}{}{}".format(mylist[i], '/', 'strategy', j, '.npy.gz'))
+
+    # Create circles based on pedestrian's positions and radius
+    patches = []
+    for k in agents_in_room:
+        circle = ptch.Circle((positions_y[time_stat_reg_start, k], -positions_x[time_stat_reg_start, k] + width),
                               radius[k])
-    patches.append(circle)
+        patches.append(circle)
 
-# Change figure settings
-ax = plt.gca()
-ax.set_xlim([5.53, 34.43])
-ax.set_ylim([-21, -5.92])
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-ax.set_aspect('equal')
+    if i == 0:
 
-# Hide the right and top spines
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
+        # Add legend
+        grid[i].add_patch(ptch.Circle((2.5,8.3), 0.25, edgecolor='black', facecolor='red', lw=0.2),)
+        grid[i].add_patch(ptch.Circle((2.5,7.3), 0.25, edgecolor='black', facecolor='blue', lw=0.2),)
+        grid[i].text(3, 8, 'Impatient', fontsize=12)
+        grid[i].text(3, 7, 'Patient', fontsize=12)
 
-# Add colored circles to represent different strategists
-p = PatchCollection(patches, cmap=cmap, norm=norm, edgecolor='black', lw=0.2)
-p.set_array(strategy[time_stat_reg_start, agents_in_room])
-ax.add_collection(p)
+        # Change figure settings
+        grid[i].set_xlim([2, 18])
+        grid[i].set_ylim([-2, 9])
+        grid[i].get_xaxis().set_visible(False)
+        grid[i].get_yaxis().set_visible(False)
+        grid[i].set_aspect('equal')
 
-# Add legend
-ax.add_patch(ptch.Circle((6.1,-6.6), 0.5, edgecolor='black', facecolor='red', lw=0.2),)
-ax.add_patch(ptch.Circle((6.1,-8), 0.5, edgecolor='black', facecolor='blue', lw=0.2),)
-ax.text(6.8, -7, 'Impatient', fontsize=20)
-ax.text(6.8, -8.5, 'Patient', fontsize=20)
+        # Add colored circles to represent different strategists
+        p = PatchCollection(patches, cmap=cmap, norm=norm, edgecolor='black', lw=0.2)
+        p.set_array(strategy[time_stat_reg_start, agents_in_room])
+        grid[i].add_collection(p)
 
-# Plot a "bottom floor"
-floor_left_x = np.arange(5.5,19.5,0.1)
-floor_left_y = -20.08*np.ones(len(floor_left_x))
-floor_right_x = np.arange(21.5,34.5,0.1)
-floor_right_y = -20.08*np.ones(len(floor_right_x))
-ax.plot(floor_left_x, floor_left_y, color='black', linewidth=2.5)
-ax.plot(floor_right_x, floor_right_y, color='black', linewidth=2.5)
+        # Set Taset parameter as figure title
+        grid[i].set_title(titles[i], fontsize=12, y=1)
 
-# Plot 3 half-circles
-x0 = 20.3
-y0 = -20
-radius0 = 9
-x = np.arange(x0-radius0,x0+radius0,0.001)
-y = np.sqrt(radius0**2-(x-x0)**2) + y0
-ax.plot(x, y, color='black', linewidth=5)
+        # Set EXIT sign
+        grid[i].text(8.8,-1.7,'EXIT', fontweight='bold', fontsize=16)
 
-x1 = 20.3
-y1 = -20
-radius1 = 6.5
-x = np.arange(x1-radius1,x1+radius1,0.001)
-y = np.sqrt(radius1**2-(x-x1)**2) + y1
-ax.plot(x, y, color='black', linewidth=5)
+        # Plot a "bottom floor"
+        floor_left_x = np.arange(0,10,0.5)
+        floor_left_y = -0.04*np.ones(20)
+        floor_right_x = np.arange(11,21,1)
+        floor_right_y = -0.04*np.ones(10)
+        grid[i].plot(floor_left_x, floor_left_y, color='black', linewidth=2)
+        grid[i].plot(floor_right_x, floor_right_y, color='black', linewidth=2)
 
-x2 = 20.3
-y2 = -20
-radius2 = 3.5
-x = np.arange(x2-radius2,x2+radius2,0.001)
-y = np.sqrt(radius2**2-(x-x2)**2) + y2
-ax.plot(x, y, color='black', linewidth=5)
+        # Figure letter
+        grid[i].text(2.1, 9.5, 'a', fontweight='bold', fontsize=14)
 
-# Plot black rectangle to represent the exit
-ax.add_patch(ptch.Rectangle((19.3,-20.73), 2.4,0.7, edgecolor='black', facecolor='black'),)
+    if i == 1:
 
-# Plot EXIT sign
-ax.text(18.85, -22, 'EXIT', fontsize=20, fontweight='bold')
+        # Add legend
+        grid[i].add_patch(ptch.Circle((2.5,8.3), 0.25, edgecolor='black', facecolor='red', lw=0.2),)
+        grid[i].add_patch(ptch.Circle((2.5,7.3), 0.25, edgecolor='black', facecolor='blue', lw=0.2),)
+        grid[i].text(3, 8, 'Impatient', fontsize=12)
+        grid[i].text(3, 7, 'Patient', fontsize=12)
 
-# Label the half-circles
-ax.text(x0-radius0-0.5, -21.4, 'A', fontsize=20, fontweight='bold')
-ax.text(x1-radius1-0.5, -21.4, 'B', fontsize=20, fontweight='bold')
-ax.text(x2-radius2-0.5, -21.4, 'C', fontsize=20, fontweight='bold')
+        # Change figure settings
+        grid[i].set_xlim([2, 18])
+        grid[i].set_ylim([-2, 9])
+        grid[i].get_xaxis().set_visible(False)
+        grid[i].get_yaxis().set_visible(False)
+        grid[i].set_aspect('equal')
+
+        # Add colored circles to represent different strategists
+        p = PatchCollection(patches, cmap=cmap, norm=norm, edgecolor='black', lw=0.2)
+        p.set_array(strategy[time_stat_reg_start, agents_in_room])
+        grid[i].add_collection(p)
+
+        # Set Taset parameter as figure title
+        grid[i].set_title(titles[i], fontsize=12, y=1)
+
+        # Set EXIT sign
+        grid[i].text(8.8,-1.7,'EXIT', fontweight='bold', fontsize=16)
+        
+        # Plot a "bottom floor"
+        floor_left_x = np.arange(0,10,0.5)
+        floor_left_y = -0.04*np.ones(20)
+        floor_right_x = np.arange(11,21,1)
+        floor_right_y = -0.04*np.ones(10)
+        grid[i].plot(floor_left_x, floor_left_y, color='black', linewidth=2)
+        grid[i].plot(floor_right_x, floor_right_y, color='black', linewidth=2)
+
+        # Figure letter
+        grid[i].text(2.1, 9.5, 'b', fontweight='bold', fontsize=14)
+
+    if i == 2:
+
+        # Add legend
+        grid[i].add_patch(ptch.Circle((2.5,8.3), 0.25, edgecolor='black', facecolor='red', lw=0.2),)
+        grid[i].add_patch(ptch.Circle((2.5,7.3), 0.25, edgecolor='black', facecolor='blue', lw=0.2),)
+        grid[i].text(3, 8, 'Impatient', fontsize=12)
+        grid[i].text(3, 7, 'Patient', fontsize=12)
+
+        # Change figure settings
+        grid[i].set_xlim([2, 18])
+        grid[i].set_ylim([-2, 9])
+        grid[i].get_xaxis().set_visible(False)
+        grid[i].get_yaxis().set_visible(False)
+        grid[i].set_aspect('equal')
+
+        # Add colored circles to represent different strategists
+        p = PatchCollection(patches, cmap=cmap, norm=norm, edgecolor='black', lw=0.2)
+        p.set_array(strategy[time_stat_reg_start, agents_in_room])
+        grid[i].add_collection(p)
+
+        # Set Taset parameter as figure title
+        grid[i].set_title(titles[i], fontsize=12, y=1)
+
+        # Set EXIT sign
+        grid[i].text(8.8,-1.7,'EXIT', fontweight='bold', fontsize=16)
+
+        # Plot a "bottom floor"
+        floor_left_x = np.arange(0,10,0.5)
+        floor_left_y = -0.04*np.ones(20)
+        floor_right_x = np.arange(11,21,1)
+        floor_right_y = -0.04*np.ones(10)
+        grid[i].plot(floor_left_x, floor_left_y, color='black', linewidth=2)
+        grid[i].plot(floor_right_x, floor_right_y, color='black', linewidth=2)
+
+        # Figure letter
+        grid[i].text(2.1, 9.5, 'c', fontweight='bold', fontsize=14)
+
+    if i == 3:
+
+        # Add legend
+        grid[i].add_patch(ptch.Circle((2.5,8.3), 0.25, edgecolor='black', facecolor='red', lw=0.2),)
+        grid[i].add_patch(ptch.Circle((2.5,7.3), 0.25, edgecolor='black', facecolor='blue', lw=0.2),)
+        grid[i].text(3, 8, 'Impatient', fontsize=12)
+        grid[i].text(3, 7, 'Patient', fontsize=12)
+
+        # Change figure settings
+        grid[i].set_xlim([2, 18])
+        grid[i].set_ylim([-2, 9])
+        grid[i].get_xaxis().set_visible(False)
+        grid[i].get_yaxis().set_visible(False)
+        grid[i].set_aspect('equal')
+
+        # Add colored circles to represent different strategists
+        p = PatchCollection(patches, cmap=cmap, norm=norm, edgecolor='black', lw=0.2)
+        p.set_array(strategy[time_stat_reg_start, agents_in_room])
+        grid[i].add_collection(p)
+
+        # Set Taset parameter as figure title
+        grid[i].set_title(titles[i], fontsize=12, y=1)
+
+        # Set EXIT sign
+        grid[i].text(8.8,-1.7,'EXIT', fontweight='bold', fontsize=16)
+
+        # Plot a "bottom floor"
+        floor_left_x = np.arange(0,10,0.5)
+        floor_left_y = -0.04*np.ones(20)
+        floor_right_x = np.arange(11,21,1)
+        floor_right_y = -0.04*np.ones(10)
+        grid[i].plot(floor_left_x, floor_left_y, color='black', linewidth=2)
+        grid[i].plot(floor_right_x, floor_right_y, color='black', linewidth=2)
+
+        # Figure letter
+        grid[i].text(2.1, 9.5, 'd', fontweight='bold', fontsize=14)
+
+# Add a black rectangle in each figure to represent the exit
+grid[0].add_patch(ptch.Rectangle((9.4,-0.41), 1.6, 0.4, edgecolor='black', facecolor='black'),)
+grid[1].add_patch(ptch.Rectangle((9.4,-0.41), 1.6, 0.4, edgecolor='black', facecolor='black'),)
+grid[2].add_patch(ptch.Rectangle((9.4,-0.41), 1.6, 0.4, edgecolor='black', facecolor='black'),)
+grid[3].add_patch(ptch.Rectangle((9.4,-0.41), 1.6, 0.4, edgecolor='black', facecolor='black'),)
 
 # Save figure as pdf
 plt.savefig('figure_2.pdf',
